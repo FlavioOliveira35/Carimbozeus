612a17a62c663193984ec732eccc1ffdbcc86b83
Flavio <flavio_cesar_oliveira@hotmail.com>
2025-09-19 01:08:59 -0300

    Mensagem descrevendo

--- sw.js
+++ sw.js
@@ -1,13 +1,12 @@
-const CACHE_NAME = 'gerador-carimbo-cache-v1';
+const CACHE_NAME = 'gerador-carimbo-cache-v2'; // Versão do cache atualizada
 const urlsToCache = [
   '/',
   '/index.html',
   '/style.css',
   '/script.js',
+  '/manifest.json',
   '/images/icons/icon-192x192.png',
-  '/images/icons/icon-512x512.png',
-  '/images/icons/favicon.ico',
-  '/images/icons/install.svg'
+  '/images/icons/icon-512x512.png'
 ];
 
 // Evento de Instalação: Abre o cache e adiciona os arquivos principais.
@@ -16,8 +15,6 @@ self.addEventListener('install', event => {
     caches.open(CACHE_NAME)
       .then(cache => {
         console.log('Cache aberto');
-        // Adiciona todos os URLs definidos ao cache.
-        // Se qualquer um falhar, a instalação do service worker falha.
         return cache.addAll(urlsToCache);
       })
   );
@@ -31,7 +28,6 @@ self.addEventListener('activate', event => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
-            // Se o cache não estiver na lista de permissões, delete-o.
             return caches.delete(cacheName);
           }
         })
